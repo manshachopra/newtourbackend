cc6d07b02452b4283532b0a43277799896ac2d29
manshachopra <manshachopra.004@gmail.com>
2025-06-21 16:53:05 +0530

    delete

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/Travelling_agency',function(err,data){
-//     if(err){
-//         console.log(err)
-//     }
-//     console.log("mongodb connected")
-// });
+// // mongoose.connect('mongodb://localhost/Travelling_agency',function(err,data){
+// //     if(err){
+// //         console.log(err)
+// //     }
+// //     console.log("mongodb connected")
+// // });
 
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb://localhost:27017/Travelling_agency', { useNewUrlParser: true, useUnifiedTopology: true }).then(res => {
+mongoose.connect('mongodb://localhost/Travelling_agency', { useNewUrlParser: true, useUnifiedTopology: true }).then(res => {
   console.log("Connected to MongoDB");
 }).catch(err => {
   console.log("Connection errr: ", err);
